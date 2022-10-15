bb7113add4c78295939305bcbf10ea2943e1c9ff
Adiwid20 <93042849+Adiwid20@users.noreply.github.com>
2022-10-16 00:06:45 +0800

    Add files via upload

---
+++ GREENFOOT/Greenfoot.Projek3/Ball.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Ball here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ball extends Actor
+{
+    /**
+     * Act - do whatever the Ball wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private int timer = 5;
+   
+    
+    public void act()
+    {
+        move(2);
+        
+    
+        
+        ballCollision();
+        
+    }
+    public void ballCollision(){
+        if (isTouching(Enemy.class)){
+            getWorld().addObject(new Duar(), getX(), getY());
+            removeTouching(Enemy.class);
+            getWorld().removeObject(this);
+        } else if(isAtEdge())
+            getWorld().removeObject(this);
+    }
+}
